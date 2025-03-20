9e2ede924e2d419d70a9552e87079f014d5eda14
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-03-20 22:23:11 +0100

    La configuración y conexioón a MongoDB es correcta.

---
+++ config/db.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+
+const connectDB = async () => {
+  try {
+    console.log('Estamos intentando conectarnos a MongoDB')
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log(`👾 MongoDB conectando en: ${mongoose.connection.host}`)
+  } catch (error) {
+    console.error(
+      `🦽 Nos está fallando la conexión a MOngoDB: ${error.message}`
+    )
+    process.exit(1)
+  }
+}
+
+export default connectDB
