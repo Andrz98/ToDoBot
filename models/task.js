3153597174057081a6110d85f3603ec8a9b75b9c
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-04-09 22:33:31 +0200

    task.js was been correct, requerid by required

--- models/task.js
+++ models/task.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 const taskSchema = new mongoose.schema({
-  userId: { type: Number, requerid: true }, // ID del usuario de telegram
+  userId: { type: Number, required: true }, // ID del usuario de telegram
   description: { type: String, required: true }, // Descripción de la tarea
   completed: { type: Boolean, default: false }, // Estado de la tarea
   reminderAt: { type: Date }, // Fecha/hora del recordatorio
