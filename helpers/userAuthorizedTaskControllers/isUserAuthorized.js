31c1249c6867fe89e771929fde8282b2c1d4733c
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-04-10 00:21:25 +0200

    renamed userAuthorized by userAuthorireztaskControllers

---
+++ helpers/userAuthorizedTaskControllers/isUserAuthorized.js
@@ -0,0 +1,17 @@
+import AuthorizedUser from '../../models/authorizedUser.js'
+
+/**
+ * Verifica si el usuario que envía el mensaje está autorizado para usar mi bot
+ *
+ * @param {Object} ctx - Contexto proporcionado por Telegraf
+ * @returns {Promise<Boolean>} - true si está autorizado, false si no
+ */
+export const isUserAuthorized = async (ctx) => {
+  const userId = ctx.from?.id
+  if (!userId) {
+    return false
+  }
+
+  const exists = await AuthorizedUser.exists({ telegramId: userId })
+  return Boolean(exists)
+}
