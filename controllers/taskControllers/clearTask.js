ac64950b7756467561ba328138f54dc5100f404e
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-04-10 00:31:28 +0200

    clearConfirm added in clearTask, secure mode

--- controllers/taskControllers/clearTask.js
+++ controllers/taskControllers/clearTask.js
@@ -2,38 +2,55 @@ import Task from '../../models/task.js'
 import { isUserAuthorized } from '../../helpers/userAuthorizedTaskControllers/isUserAuthorized.js'
 
 /**
- * Controlador para solicitar la confirmación antes de eliminar todas las tareas /clear
+ * Controlador para manejar /clear y /confirmclear
  *
- *  @param {object} ctx - Objeto de contexo proporcionado por telegraf
+ * @param {object} ctx - Objeto de contexo proporcionado por telegraf
  */
 
 export const clearTask = async (ctx) => {
   try {
-    // Extraigo el ID del usuario que envió el comando
+    // Extraigo el comando recibido y el ID del usuario
     const userId = ctx.from.id
+    const command = ctx.message.text.trim()
 
     // Verifico si el usuario esta autorizado a usar el bot
     if (!(await isUserAuthorized(ctx))) {
       return ctx.reply('🥸 Debes estar autorizado para usar este bot.')
     }
 
-    // Consulto si el usuario tiene tareas activas
-    const taskCount = await Task.countDocuments({ userId })
+    // /clear → Solicitud de confirmación
+    if (command === '/clear') {
+      const taskCount = await Task.countDocuments({ userId })
 
-    // Si no tiene tareas activas, respondemos con un mensaje informativo
-    if (taskCount === 0) {
-      return ctx.reply('🤯 No tienes tareas activas que eliminar.')
+      if (taskCount === 0) {
+        return ctx.reply('🤯 No tienes tareas activas que eliminar.')
+      }
+
+      return ctx.reply(
+        '🤯 Estás a punto de eliminar TODAS tus tareas.\n' +
+          'Si estás completamente seguro, escribe el comando:\n/confirmclear'
+      )
     }
 
-    // Envío una advertencia al usuario para confirmar la eliminación de todas las tareas
-    return ctx.reply(
-      '🤯 Estás a punto de eliminar TODAS tus tareas.\n' +
-        'Si estás completamente seguro, escribe el comando:\n/confirmclear'
-    )
+    // /confirmclear → Eliminación real de las tareas
+    if (command === '/confirmclear') {
+      const taskCount = await Task.countDocuments({ userId })
+
+      if (taskCount === 0) {
+        return ctx.reply('🤯 No tienes tareas activas que eliminar.')
+      }
+
+      await Task.deleteMany({ userId })
+
+      return ctx.reply(
+        `🫡 Se han eliminado tus ${taskCount} tareas. ¡Empezamos de cero!`
+      )
+    }
+
+    // Si el comando no es reconocido dentro de este handler
+    return ctx.reply('🤯 Comando no válido. Usa /clear para empezar.')
   } catch (error) {
-    console.error(
-      `😵‍💫 Error en confirmación de borrado masivo: ${error.message}`
-    )
+    console.error(`😵‍💫 Error al gestionar comando clear: ${error.message}`)
     ctx.reply(
       '😵‍💫 Ocurrió un error al procesar tu solicitud. Intenta más tarde.'
     )
