4804a49ba40653b3631a3a669945df1dbedf4718
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-04-10 19:35:20 +0200

    I have now created completeTask component, right now the user can do complete a task within the chat

---
+++ controllers/taskControllers/completeTask.js
@@ -0,0 +1,29 @@
+import { findUserTaskByName } from '../../helpers/userTaskBynameController/findUserTaskByName.js'
+
+/**
+ * Controlador para marcar una tarea como completada /done
+ *
+ * @param {object} ctx - Objeto de contexo proporcionado por telegraf
+ */
+
+export const completeTask = async (ctx) => {
+  try {
+    // Utilizo el helper que valida la autorización existente de la tarea
+    const { error, task } = await findUserTaskByName(ctx)
+    if (error) {
+      return ctx.reply(error)
+    }
+
+    // Marco la tarea como completada
+    task.completed = true
+    await task.save()
+
+    // Confirmo que la acción de completar la tarea fue exitosa
+    return ctx.reply(
+      `🫡 Ojitooo finalizaste la tarea "${task.name}", sigue así.`
+    )
+  } catch (error) {
+    console.error(`😵‍💫 Error al completar la tarea: ${error.message}`)
+    ctx.reply('😵‍💫 Ocurrio un error al completar la tarea. Intenta mas tarde.')
+  }
+}
