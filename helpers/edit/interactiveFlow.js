efe534cabb5eac2cdba69696c2b8ed1a406aecfb
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-04-22 22:50:41 +0200

    C:/Program Files/Git/edit corrected

--- helpers/edit/interactiveFlow.js
+++ helpers/edit/interactiveFlow.js
@@ -18,12 +18,12 @@ export const buildEditMenu = (task, timeZone) => {
 
   const text = `<b>${name}</b>\n` + `${descriptionText}\n` + `🔹 ${dateText}`
 
-  const markup = Markup.inlineKeyboard([
+  const inline = Markup.inlineKeyboard([
     [Markup.button.callback('✔️ Nombre', 'edit_name')],
     [Markup.button.callback('🔸 Descripción', 'edit_desc')],
     [Markup.button.callback('🔹 Fecha', 'edit_date')],
     [Markup.button.callback('✖️ Cancelar', 'edit_cancel')]
   ])
 
-  return { text, markup: markup.reply_markup }
+  return { text, markup: { reply_markup: inline.reply_markup } }
 }
