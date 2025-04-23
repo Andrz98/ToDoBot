8b73d6815b8ba649e092e8e6d37df555f98342b2
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-04-24 01:19:04 +0200

    debugging

--- helpers/interactive/taskSelector.js
+++ helpers/interactive/taskSelector.js
@@ -1,4 +1,4 @@
-import { findTaskForController } from '../userTaskBynameController/findTaskForController'
+import { findTaskForController } from '../userTaskBynameController/findTaskForController.js'
 
 /**
  * Registra un handler para cualquier callback del tipo `${prefix}_{taskId}`
