4856b1374f86e30f72667123a2afe83276b4c124
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-04-24 19:20:10 +0200

    debugging

--- controllers/taskControllers/completeTask.js
+++ controllers/taskControllers/completeTask.js
@@ -1,20 +1,12 @@
-import { findTaskForController } from '../../helpers/userTaskBynameController/findTaskForController.js'
 import { isUserAuthorized } from '../../helpers/userAuthorizedTaskController/isUserAuthorized.js'
+import { findTask } from '../../helpers/tasks/findTask.js'
 
 /**
  * Controlador para marcar una tarea como completada /done
- *
- * Formato obligatorio:
- * /done NombreExactoDeLaTarea
- *
- * @param {object} ctx - Objeto de contexto proporcionado por telegraf
  */
 export const completeTask = async (ctx) => {
   try {
-    // Validación de contexto
-
     const userId = ctx.from.id
-
     const rawText = ctx.message?.text
     if (!rawText) {
       return ctx.reply(
@@ -22,41 +14,38 @@ export const completeTask = async (ctx) => {
       )
     }
 
-    const input = rawText.replace(/^\/done\s*/i, '').trim()
-
-    // Validación específica para cuando el usuario solo envía el comando sin parámetros
-    if (!input || input === ':') {
+    const taskName = rawText.replace(/^\/done\s*/i, '').trim()
+    if (!taskName) {
       return ctx.reply(
         '🧾 <b>Formato correcto:</b>\n/done NombreExactoDeLaTarea\n\n<b>Ejemplo:</b>\n/done Comprar pan',
         { parse_mode: 'HTML' }
       )
     }
 
-    // Verifico si el usuario está autorizado a usar el bot
     if (!(await isUserAuthorized(ctx))) {
       return ctx.reply('🥸 Debes estar autorizado para usar este bot.')
     }
 
-    const taskName = input
-
-    const task = await findTaskForController(userId, taskName)
+    const task = await findTask(userId, { name: taskName })
     if (!task) {
-      return ctx.reply(`🤯 No se encontró ninguna tarea llamada "${taskName}"`)
+      return ctx.reply(`🤯 No se encontró ninguna tarea llamada "${taskName}".`)
     }
 
     if (task.completed) {
-      return ctx.reply('Esta tarea ya ha sido completada previamente.')
+      return ctx.reply('⚠️ Esta tarea ya está completada.')
     }
 
     task.completed = true
     await task.save()
 
     return ctx.reply(
-      `Perfecto,¡Tarea completada!\n<b>${task.name}</b> ha sido marcada como finalizada.`,
+      `Perfecto, ¡tarea completada!\n<b>${task.name}</b> ha sido marcada como finalizada.`,
       { parse_mode: 'HTML' }
     )
   } catch (error) {
     console.error('😵‍💫 Error al completar la tarea:', error)
-    ctx.reply('😵‍💫 Ocurrió un error al completar la tarea. Intenta más tarde.')
+    return ctx.reply(
+      '😵‍💫 Ocurrió un error al completar la tarea. Intenta más tarde.'
+    )
   }
 }
