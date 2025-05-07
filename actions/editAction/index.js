a0762a673259a7fad94760271e7658ecedbd90ff
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-05-08 01:10:29 +0200

    editAction is modularizated

--- actions/editAction/index.js
+++ actions/editAction/index.js
@@ -1,8 +1,17 @@
+// actions/editAction/index.js
+
 import { registerStartEditAction } from './startEditAction.js'
 import { registerFieldEditActions } from './fieldEditActions.js'
 import { registerMessageEditHandler } from './messageEditHandler.js'
 import { registerSaveEditAction } from './saveEditAction.js'
 
+/**
+ * Agrega al bot todos los pasos del flujo /edit:
+ * 1) Arrancar flujo (/edit + selector)
+ * 2) Manejar selección de campo
+ * 3) Capturar respuestas force-reply
+ * 4) Guardar cambios
+ */
 export function registerEditActions(bot) {
   registerStartEditAction(bot)
   registerFieldEditActions(bot)
