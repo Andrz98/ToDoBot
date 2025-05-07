448a5a0560e4aae4aa2f1cf219d8cfd2b79c21db
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-05-07 21:44:23 +0200

    debugging

--- controllers/taskControllers/addTask.js
+++ controllers/taskControllers/addTask.js
@@ -1,11 +1,18 @@
 import { isUserAuthorized } from '../../helpers/userAuthorizedTaskController/isUserAuthorized.js'
 import { buildAddButton } from '../../helpers/taskHelpers/add/addCommand.js'
-import { genericReplyMessages } from '../../helpers/replyMessages/genericReplyMessages.js'
+import { replyMessages } from '../../helpers/replyMessages/genericReplyMessages.js'
 
+/**
+ * Controlador para /add que lanza el botón inicial de creación.
+ * @param {import('telegraf').Context} ctx
+ */
 export const addTask = async (ctx) => {
+  // 1) Autorización
   if (!(await isUserAuthorized(ctx))) {
-    return genericReplyMessages.unauthorizedUser(ctx)
+    return replyMessages.unauthorized(ctx)
   }
+
+  // 2) Mostramos el botón inicial para crear tarea
   const { text, markup } = buildAddButton()
   return ctx.reply(text, markup)
 }
