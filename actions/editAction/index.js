3029ad712bc3fbd5f790e2ae47b8b37653af96cd
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-05-27 01:21:25 +0200

    I´m solving a new problem with /edit, this command is not working. When the user push the button can´t interactive with the list of task to do an edit action

--- actions/editAction/index.js
+++ actions/editAction/index.js
@@ -2,7 +2,6 @@
 
 import { registerStartEditAction } from './startEditAction.js'
 import { registerFieldEditActions } from './fieldEditActions.js'
-import { registerMessageEditHandler } from './messageEditHandler.js'
 import { registerSaveEditAction } from './saveEditAction.js'
 
 /**
@@ -15,6 +14,5 @@ import { registerSaveEditAction } from './saveEditAction.js'
 export function registerEditActions(bot) {
   registerStartEditAction(bot)
   registerFieldEditActions(bot)
-  registerMessageEditHandler(bot)
   registerSaveEditAction(bot)
 }
