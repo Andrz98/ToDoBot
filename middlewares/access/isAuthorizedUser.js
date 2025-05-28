c49ecb4065b06f5066a0f6cc57e033650c281fb8
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-05-28 02:10:17 +0200

    Debugging with telegraf-2

--- middlewares/access/isAuthorizedUser.js
+++ middlewares/access/isAuthorizedUser.js
@@ -1,14 +1,15 @@
 import { isUserAuthorized } from '../../helpers/userAuthorizedTaskController/isUserAuthorized.js'
 
-export const isAuthorizedUser = async (ctx, next) => {
-  try {
-    const authorized = await isUserAuthorized(ctx)
-    if (!authorized) {
-      return ctx.reply('🥸 Debes estar autorizado para usar este bot.')
-    }
-    return next()
-  } catch (error) {
-    console.error('Error al verificar autorización:', error)
-    return ctx.reply('😵‍💫 Error interno de autorización.')
-  }
+export function isAuthorizedUser(ctx, next) {
+  isUserAuthorized(ctx)
+    .then((authorized) => {
+      if (!authorized) {
+        return ctx.reply('🥸 Debes estar autorizado para usar este bot.')
+      }
+      return next()
+    })
+    .catch((error) => {
+      console.error('❌ Error al verificar autorización:', error)
+      return ctx.reply('😵‍💫 Error interno de autorización.')
+    })
 }
